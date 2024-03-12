23874b60b68cd4c72cf49ce9133e9e9c2fe14872
gukho <hassamnet@gmail.com>
2024-03-12 22:33:44 +0900

    Add card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,44 @@
 import { FC } from 'react';
 
+const Grade: FC<{ number: number }> = ({ number }) => {
+  return (
+    <div className="grade-wrapper">
+      {Array(5)
+        .fill(0)
+        .map((v, index) => (
+          <div className={`grade${index < number ? ' scored' : ''}`}></div>
+        ))}
+    </div>
+  );
+};
+
 interface CardProps {
+  title: string;
   author?: string;
   label?: string;
-  title?: string;
   grade?: number; // 1~5 정수 별점
   description?: string;
   landscape?: boolean; // 가로형 여부
 }
 
 const Card: FC<CardProps> = ({
+  title,
   author,
   label,
-  title,
   grade,
   description,
   landscape,
 }) => {
   return (
-    <div className={`card ${landscape ? 'landscape' : ''}`}>
-      <div></div>
-      <div></div>
+    <div className={`card${landscape ? ' landscape' : ''}`}>
+      <img src="https://source.unsplash.com/random" />
+      <div className="detail-container">
+        {label && <div className="label">{label}</div>}
+        <div className="title">{title}</div>
+        {author && <div className="author">{author}</div>}
+        {grade && <Grade number={grade} />}
+        {description && <div className="description">{description}</div>}
+      </div>
     </div>
   );
 };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,10 +5,27 @@ const Home = () => {
   return (
     <div className="page-container">
       <div className="container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          label="Card Label"
+          title="Card Title1"
+          author="author1"
+          grade={3}
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        />
+        <Card
+          label="Card Label"
+          title="Card Title2"
+          author="author2"
+          grade={4}
+        />
+        <Card label="Card Label" title="Card Title3" author="author3" />
+        <Card
+          landscape
+          title="Card Title4"
+          author="author2"
+          grade={4}
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        />
       </div>
       <div className="container">
         <TextInput />
